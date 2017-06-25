056ca4d0e959a3a7e39e13e6bd24141edf9dbf73
Ruxo Zheng <smartgit@just.sent.as>
2017-06-25 10:02:02 +0700

    Add Wsdl project

---
+++ Source/SoapHttpClient/Wsdl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace SoapHttpClient
+{
+    public struct SoapFuncArgs
+    {
+        public XElement Body;
+        public XElement Header;
+    }
+
+    class WsdlOperation
+    {
+        public string Name;
+    }
+    class WsdlPort
+    {
+        public string Name;
+        public Uri Endpoint;
+    }
+    class WsdlService
+    {
+        public string Name;
+        public Dictionary<string,WsdlPort> Ports;
+    }
+    public class Wsdl
+    {
+        readonly XDocument wsdl;
+        public Wsdl(XDocument wsdl) {
+            this.wsdl = wsdl ?? throw new ArgumentNullException("wsdl");
+        }
+
+        public SoapFuncArgs CallFunc(string functionName, object args) {
+            return new SoapFuncArgs();
+        }
+    }
+}
